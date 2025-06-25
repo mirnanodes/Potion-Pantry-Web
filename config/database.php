cce404875bf03defa2fd5b0e6ac94357a2555435
mirnanodes <formeandgame78@gmail.com>
2025-06-26 01:43:22 +0700

    muts

--- config/database.php
+++ config/database.php
@@ -1,15 +1,16 @@
 <?php
 // Database configuration for Potion Pantry
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'ppw-mirna-uas-potionPantry');
-define('DB_USER', 'root');
-define('DB_PASS', ''); // Biasanya kosong untuk XAMPP
+define('DB_NAME', 'u985354573_potionPantry');
+define('DB_USER', 'u985354573_ayu_mirna');
+define('DB_PASS', 'Miwa200531');
 
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    echo("ganteng");
 } catch(PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
